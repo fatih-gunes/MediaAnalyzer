7f4347557447b8f89afbb85d36a4bdde99c31ba4
fatih <fatih.gunes@yahoo.com>
2023-09-18 12:08:37 +0200

    Initial commit

---
+++ src/test/java/com/castlabs/gunes/mediaanalyzer/MediaAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.castlabs.gunes.mediaanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
